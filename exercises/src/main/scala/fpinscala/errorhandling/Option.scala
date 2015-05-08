53573cd764b7fe98924af8eb7a7dcb96be27ac27
krrrr38 <k.kaizu38@gmail.com>
2015-05-08 21:44:02 +0900

    Solve errorhandling exercises

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -4,20 +4,37 @@ package fpinscala.errorhandling
 import scala.{Option => _, Some => _, Either => _, _} // hide std library `Option`, `Some` and `Either`, since we are writing our own in this chapter
 
 sealed trait Option[+A] {
-  def map[B](f: A => B): Option[B] = sys.error("todo")
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(get) => Some(f(get))
+  }
 
-  def getOrElse[B>:A](default: => B): B = sys.error("todo")
+  def getOrElse[B>:A](default: => B): B = this match {
+    case None => default
+    case Some(get) => get
+  }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = sys.error("todo")
+  def flatMap[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(get) => f(get)
+  }
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = sys.error("todo")
+  def orElse[B>:A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case _ => this
+  }
 
-  def filter(f: A => Boolean): Option[A] = sys.error("todo")
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(get) if f(get) => this
+    case _ => None
+  }
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
 
 object Option {
+  def apply[A](a: A): Option[A] = if (a != null) Some(a) else None
+
   def failingFn(i: Int): Int = {
     val y: Int = throw new Exception("fail!") // `val y: Int = ...` declares `y` as having type `Int`, and sets it equal to the right hand side of the `=`.
     try {
@@ -38,11 +55,34 @@ object Option {
   def mean(xs: Seq[Double]): Option[Double] =
     if (xs.isEmpty) None
     else Some(xs.sum / xs.length)
-  def variance(xs: Seq[Double]): Option[Double] = sys.error("todo")
+  def variance(xs: Seq[Double]): Option[Double] = {
+    mean(xs) map { _mean =>
+      ???
+    }
+  }
 
-  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] = sys.error("todo")
+  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+    for { va <- a; vb <- b } yield f(va, vb)
 
-  def sequence[A](a: List[Option[A]]): Option[List[A]] = sys.error("todo")
+//  def map2[A,B,C](a: Option[A], b: Option[B])(f: (A, B) => C): Option[C] =
+//    a.flatMap(va => b.map(vb => f(va, vb)))
 
-  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = sys.error("todo")
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = a match {
+    case Nil => Some(Nil)
+    case head::tail => head.flatMap(v => sequence(tail) map (v :: _))
+  }
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] =
+    sequence(a.map(f))
+}
+
+object OptionMain {
+  import Option._
+
+  def main(args: Array[String]) {
+    println("--- All sequences return true.")
+    println(sequence(List(Some(1))) == Some(List(1)))
+    println(sequence(List(Some(1), Some(2))) == Some(List(1, 2)))
+    println(sequence(List(Some(1), None, Some(2))) == None)
+  }
 }
